5f3a2a7ac08c3edb1006fb93341cf40278e02dcd
YDH95 <wowhky81234@gmail.com>
2022-12-26 23:58:24 +0900

    remove and toggle

--- src/App.js
+++ src/App.js
@@ -39,10 +39,27 @@ const App = () => {
     [todos],
   );
 
+  const onRemove = useCallback((id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  });
+
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id
+            ? { ...todo, checked: !todo.checked }
+            : { ...todo, checked: todo.checked },
+        ),
+      );
+    },
+    [todos],
+  );
+
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
 };
